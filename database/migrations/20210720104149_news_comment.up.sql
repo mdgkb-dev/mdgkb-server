23f57c6372f0166acc03345498187efbaaf6b1c8
lakkinzimusic <lakkinzimusic@gmail.com>
2021-07-22 22:15:34 +0300

    Add lost migrations

---
+++ database/migrations/20210720104149_news_comment.up.sql
@@ -0,0 +1,6 @@
+CREATE TABLE news_comments (
+                            id uuid DEFAULT uuid_generate_v4() NOT NULL PRIMARY KEY,
+                            news_id uuid  REFERENCES news (id) ON UPDATE CASCADE ON DELETE CASCADE,
+                            text TEXT,
+                            published_on   date
+);
