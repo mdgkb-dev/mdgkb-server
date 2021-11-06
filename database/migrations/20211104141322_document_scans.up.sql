655d33a7bba98a31ac97bbd01013b65566114556
lakkinzimusic <lakkinzimusic@gmail.com>
2021-11-06 14:57:27 +0300

    VacanciesResponse drafy

---
+++ database/migrations/20211104141322_document_scans.up.sql
@@ -0,0 +1,8 @@
+create table document_scans
+(
+    id uuid default uuid_generate_v4() not null
+        constraint "PK_2e1aa55eac1947ddf3221506edb"
+            primary key,
+    document_id uuid not null references documents,
+    scan_id uuid  REFERENCES file_infos(id) ON UPDATE CASCADE ON DELETE CASCADE
+);
