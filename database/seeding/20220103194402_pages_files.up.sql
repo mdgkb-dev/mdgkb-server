af252bae4d4095dd799d3d5f330e0cde67f2c9ca
LakkinziMusic <lakkinzimusic@gmail.com>
2022-01-03 22:50:40 +0300

    More pages

---
+++ database/seeding/20220103194402_pages_files.up.sql
@@ -0,0 +1,4 @@
+insert into file_infos (id, original_name, file_system_path)
+values  ('e33d8599-64e2-43e2-9724-aa2a207c8d14', '', '933/951/e33d8599-64e2-43e2-9724-aa2a207c8d14'),
+        ('e780c64d-4f77-491a-a3c6-5eddfa4018b1', '', '198/139/e780c64d-4f77-491a-a3c6-5eddfa4018b1'),
+        ('4703cb06-2bb3-4cba-9677-16cc06e6eea0', '', '250/392/4703cb06-2bb3-4cba-9677-16cc06e6eea0');
