eba33b461c97bf56654c8e4611a23a8cc68a1b9c
Daniil Gayvoronskiy <qwer.82@mail.ru>
2022-02-06 00:54:11 +0300

    Add item contacts for submenu About us

--- database/seeding/20210930131222_sub_menu.up.sql
+++ database/seeding/20210930131222_sub_menu.up.sql
@@ -23,4 +23,5 @@ VALUES
         ('ce08a0da-65d3-44d6-aaf0-0dd573c887e1', 'Правила для доноров', '/donor-rules', NULL, 5, NULL, NULL, '4f6836fe-36eb-440d-8c02-5dd9307f7620'),
         ('6d4a9470-83d8-4b3e-9be7-71d2a7e1f3a9', 'Платные медицинские услуги', '/paid-services', NULL, 6, NULL, NULL, '4f6836fe-36eb-440d-8c02-5dd9307f7620'),
         ('6d4a9470-83d8-4b3e-9be7-71d2a7e1f3a8', 'Платные программы', '/paid-programs', NULL, 7, NULL, NULL, '4f6836fe-36eb-440d-8c02-5dd9307f7620'),
-        ('d5610693-f87a-4b68-8588-a95b71ed40d6', 'Наши партнеры', '/partners', NULL, 1, NULL, NULL, 'd147efe7-7ab5-4d37-99f2-11a47a4d1e24');
+        ('d5610693-f87a-4b68-8588-a95b71ed40d6', 'Наши партнеры', '/partners', NULL, 1, NULL, NULL, 'd147efe7-7ab5-4d37-99f2-11a47a4d1e24'),
+        ('0545784e-f27a-4007-8673-14588ed2aea8', 'Контакты', '/contacts', NULL, 1, NULL, NULL, 'd147efe7-7ab5-4d37-99f2-11a47a4d1e24');
