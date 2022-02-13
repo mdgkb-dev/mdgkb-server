77197640a4ab3fec4ceb2c19c228ec2e2744693c
LakkinziMusic <lakkinzimusic@gmail.com>
2022-02-13 13:21:07 +0300

    Weekday shortname

--- models/Weekday.go
+++ models/Weekday.go
@@ -3,9 +3,10 @@ package models
 import "github.com/google/uuid"
 
 type Weekday struct {
-	ID     uuid.UUID `bun:"type:uuid,default:uuid_generate_v4()" json:"id" `
-	Number int       `json:"number"`
-	Name   string    `json:"name"`
+	ID        uuid.UUID `bun:"type:uuid,default:uuid_generate_v4()" json:"id" `
+	Number    int       `json:"number"`
+	Name      string    `json:"name"`
+	ShortName string    `json:"shortName"`
 }
 
 type Weekdays []*Weekday
