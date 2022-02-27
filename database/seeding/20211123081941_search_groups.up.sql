9651493f9fc0aeafc6bf7b323c5ce6cfd48b98a5
LakkinziMusic <lakkinzimusic@gmail.com>
2022-02-27 19:15:11 +0300

    Filters vacancies draft

--- database/seeding/20211123081941_search_groups.up.sql
+++ database/seeding/20211123081941_search_groups.up.sql
@@ -4,4 +4,5 @@ values  ('486303cb-1b02-4059-a860-25704dcab8ea', 'doctor', 2, '/doctors', 'docto
         ('4f4cf39a-1125-4989-8c11-b4335517f31d', 'position', 4, '/positions', 'positions', 'name', 'Должности', 'name', 'id'),
         ('f68d3d90-60a0-45d1-a8fd-0d8d88a18eca', 'paidService', 3, '/paid-services', 'paid_services', 'name', 'Платные услуги', 'name', 'id'),
         ('76250b93-b54a-42a5-a6f8-3ea517960525', 'teacher', null, null, 'teachers_view', 'full_name', null, 'full_name', 'id'),
-        ('ffaf957c-ea44-4f47-8ae7-a817c58a0348', 'dpoCourse', null, null, 'dpo_courses', 'name', null, 'name', 'id');
+        ('ffaf957c-ea44-4f47-8ae7-a817c58a0348', 'dpoCourse', null, null, 'dpo_courses', 'name', null, 'name', 'id'),
+        ('d8631b3a-79e9-4ec8-be98-58e77a199371', 'vacancy', null, '/vacancies', 'vacancies', 'title', null, 'title', 'slug');
