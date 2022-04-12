26a3ddf0d0da4603faadd739af49523758ba1f02
LakkinziMusic <lakkinzimusic@gmail.com>
2022-04-12 21:43:17 +0300

    Post alias restore

--- models/PostgraduateCourse.go
+++ models/PostgraduateCourse.go
@@ -7,7 +7,7 @@ import (
 )
 
 type PostgraduateCourse struct {
-	bun.BaseModel                               `bun:"postgraduate_courses,select:postgraduate_courses_view,alias:pcv"`
+	bun.BaseModel                               `bun:"postgraduate_courses,select:postgraduate_courses_view,alias:postgraduate_courses_view"`
 	ID                                          uuid.NullUUID                      `bun:"type:uuid,default:uuid_generate_v4()" json:"id"`
 	Description                                 string                             `json:"description"`
 	EducationForm                               string                             `json:"educationForm"`
