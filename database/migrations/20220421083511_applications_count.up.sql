20fc102e2a913ea8aea1b03642c9f58ea0275235
LakkinziMusic <lakkinzimusic@gmail.com>
2022-04-22 14:19:31 +0300

    Applications Count fix

--- database/migrations/20220421083511_applications_count.up.sql
+++ database/migrations/20220421083511_applications_count.up.sql
@@ -1,12 +1,12 @@
 CREATE OR REPLACE view applications_counts as
-select count(fv.id) as new_applications, 'dpo_applications' as key from form_values as fv
-        join dpo_applications da on fv.id = da.form_value_id
+select count(fv.id) as count, 'dpo_applications' as table_name from form_values as fv
+                                                                        join dpo_applications da on fv.id = da.form_value_id
 where fv.is_new = true
 union
-select count(fv.id) as new_applications, 'postgraduate_applications' as key from form_values as fv
-        join postgraduate_applications pa on fv.id = pa.form_value_id
+select count(fv.id) as count, 'postgraduate_applications' as table_name from form_values as fv
+                                                                                 join postgraduate_applications pa on fv.id = pa.form_value_id
 where fv.is_new = true
 union
-select count(fv.id) as new_applications, 'residency_applications' as key from form_values as fv
-        join residency_applications ra on fv.id = ra.form_value_id
+select count(fv.id) as count, 'residency_applications' as table_name from form_values as fv
+                                                                              join residency_applications ra on fv.id = ra.form_value_id
 where fv.is_new = true
