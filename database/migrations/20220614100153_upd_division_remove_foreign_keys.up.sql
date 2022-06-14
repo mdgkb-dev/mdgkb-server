63ce5a8bf762ac9823c796c796a4cd842d0bbe5f
Daniil Gayvoronskiy <qwer.82@mail.ru>
2022-06-14 13:40:49 +0300

    Add_Division_Chief_in_admin

---
+++ database/migrations/20220614100153_upd_division_remove_foreign_keys.up.sql
@@ -0,0 +1,16 @@
+
+alter table divisions drop constraint divisions_contact_info_id_fkey;
+
+alter table divisions drop constraint divisions_entrance_id_fkey;
+
+alter table divisions drop constraint divisions_floor_id_fkey;
+
+alter table divisions drop constraint divisions_hospitalization_contact_info_id_fkey;
+
+alter table divisions drop constraint divisions_hospitalization_doctor_id_fkey;
+
+alter table divisions drop constraint divisions_schedule_id_fkey;
+
+alter table divisions drop constraint divisions_timetable_id_fkey;
+
+alter table divisions drop constraint divisions_treat_direction_id_fkey;
