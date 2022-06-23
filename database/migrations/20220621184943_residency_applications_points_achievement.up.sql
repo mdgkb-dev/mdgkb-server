46d4c2b820eaf7f132e7e1b80399f271249fea22
LakkinziMusic <lakkinzimusic@gmail.com>
2022-06-23 19:02:46 +0300

    Typo hotfix

--- database/migrations/20220621184943_residency_applications_points_achievement.up.sql
+++ database/migrations/20220621184943_residency_applications_points_achievement.up.sql
@@ -10,7 +10,7 @@ CREATE TABLE residency_applications_points_achievement
 (
     id uuid DEFAULT uuid_generate_v4() NOT NULL PRIMARY KEY,
     name varchar,
-    residency_application_id  uuid  REFERENCES residency_applications(id) ON UPDATE CASCADE ON DELETE CASCADE   t,
+    residency_application_id  uuid  REFERENCES residency_applications(id) ON UPDATE CASCADE ON DELETE CASCADE,
     points_achievement_id  uuid  REFERENCES points_achievements(id) ON UPDATE CASCADE ON DELETE CASCADE,
     file_info_id uuid  REFERENCES file_infos (id) ON UPDATE CASCADE ON DELETE CASCADE DEFAULT NULL,
     approved boolean
