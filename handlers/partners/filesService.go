0d65092da67a9fc254dc56cbc4d0dd44e4ac4ff2
LakkinziMusic <lakkinzimusic@gmail.com>
2022-06-25 16:26:28 +0300

    Upload fix

--- handlers/partners/filesService.go
+++ handlers/partners/filesService.go
@@ -1,31 +1,14 @@
 package partners
 
 import (
-	"errors"
 	"github.com/gin-gonic/gin"
 	"mdgkb/mdgkb-server/models"
 	"mime/multipart"
-	"os"
-	"path/filepath"
-	"strings"
 )
 
 func (s *FilesService) Upload(c *gin.Context, item *models.Partner, files map[string][]*multipart.FileHeader) (err error) {
 	for i, file := range files {
-		path := item.SetFilePath(&i)
-		if path == nil {
-			return errors.New("file does not relate to anything")
-		}
-		uploadPath := "/home/lakkinzimusic/prog/mdgkb/mdgkb-server/static"
-		pathDirs := strings.Split(*path, string(os.PathSeparator))
-		pathToFile := filepath.Join(uploadPath, filepath.Join(pathDirs[:len(pathDirs)-1]...))
-		err = os.MkdirAll(pathToFile, os.ModePerm)
-		if err != nil {
-			return err
-		}
-
-		fullPath := filepath.Join(uploadPath, *path)
-		err = c.SaveUploadedFile(file[0], fullPath)
+		err = s.helper.Uploader.Upload(c, file, item.SetFilePath(&i))
 		if err != nil {
 			return err
 		}
