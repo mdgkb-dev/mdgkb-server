9373c86a0bfc591e9dbd03f9cf9c9aba4b58c54f
Daniel <qwer.82@mail.ru>
2022-08-06 01:27:57 +0300

    upd_viewedAnswer_in_profile

--- routing/questions/routing.go
+++ routing/questions/routing.go
@@ -14,7 +14,7 @@ func Init(r *gin.RouterGroup, h handler.IHandler) {
 	r.DELETE("/:id", h.Delete)
 	r.PUT("/:id", h.Update)
 	r.PUT("/new/:id", h.ChangeNewStatus)
-	r.PUT("/read-answers/:user-id", h.ReadAnswers)
+	r.PUT("/profile/question-answer/:user-id", h.ReadAnswers)
 	r.PUT("/publish/:id", h.Publish)
 	r.PUT("/many", h.UpsertMany)
 }
