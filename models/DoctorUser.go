362575f6deb14c6c218d0eb699271b288467da77
LakkinziMusic <lakkinzimusic@gmail.com>
2022-08-23 16:07:57 +0300

    User uuid hotfix

--- models/DoctorUser.go
+++ models/DoctorUser.go
@@ -7,11 +7,11 @@ import (
 
 type DoctorUser struct {
 	bun.BaseModel `bun:"doctors_users,alias:doctors_users"`
-	ID            uuid.UUID `bun:"id,pk,type:uuid,default:uuid_generate_v4()" json:"id" `
-	Doctor        *Doctor   `bun:"rel:belongs-to" json:"doctor"`
-	DoctorID      uuid.UUID `bun:"type:uuid" json:"doctorId"`
-	User          *User     `bun:"rel:belongs-to" json:"user"`
-	UserID        uuid.UUID `bun:"type:uuid" json:"userId"`
+	ID            uuid.NullUUID `bun:"id,pk,type:uuid,default:uuid_generate_v4()" json:"id" `
+	Doctor        *Doctor       `bun:"rel:belongs-to" json:"doctor"`
+	DoctorID      uuid.NullUUID `bun:"type:uuid" json:"doctorId"`
+	User          *User         `bun:"rel:belongs-to" json:"user"`
+	UserID        uuid.NullUUID `bun:"type:uuid" json:"userId"`
 }
 
 type DoctorsUsers []*DoctorUser
