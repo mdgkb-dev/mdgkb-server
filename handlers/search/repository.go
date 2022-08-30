0e17d87796932c6518a32c8bdba56c23e98fcade
LakkinziMusic <lakkinzimusic@gmail.com>
2022-08-30 13:39:30 +0300

    Search fix

--- handlers/search/repository.go
+++ handlers/search/repository.go
@@ -36,7 +36,8 @@ func (r *Repository) search(searchModel *models.SearchModel) error {
 		Model(&searchModel.SearchGroup.SearchElements).
 		ColumnExpr("id, ? as value, ? as label", bun.Ident(searchModel.SearchGroup.ValueColumn), bun.Ident(searchModel.SearchGroup.LabelColumn)).
 		ModelTableExpr(searchModel.SearchGroup.Table).
-		Where("lower(regexp_replace(?, '[^а-яА-Яa-zA-Z0-9 ]', '', 'g')) LIKE lower(?)", searchModel.SearchGroup.SearchColumn, "%"+search+"%").
+		Where("lower(regexp_replace(?, '[^а-яА-Яa-zA-Z0-9 ]', '', 'g')) LIKE lower(?)", bun.Ident(searchModel.SearchGroup.SearchColumn), "%"+search+"%").
+		Order(searchModel.SearchGroup.SearchColumn).
 		Scan(r.ctx)
 	return err
 }
