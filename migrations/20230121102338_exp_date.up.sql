9f341238ae6039e8a7f9961789cf3e541b539fa9
LakkinziMusic <lakkinzimusic@gmail.com>
2023-01-23 22:59:07 +0300

    MigrateFix

--- migrations/20230121102338_exp_date.up.sql
+++ migrations/20230121102338_exp_date.up.sql
@@ -2,11 +2,10 @@ alter table experiences
     rename column experience_start to item_start;
 
 alter table experiences
-    alter column "start" type date using to_date("start"::text, 'YYYY');
+    alter column item_start type date using to_date(item_start::text, 'YYYY');
 
 alter table experiences
     rename column experience_end to "item_end";
 
 alter table experiences
-    alter column "end" type date using to_date("end"::text, 'YYYY');
-
+    alter column item_end type date using to_date(item_end::text, 'YYYY');
