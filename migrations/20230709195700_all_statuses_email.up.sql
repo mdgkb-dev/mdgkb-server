44252b3b3ca4e606c4cc0916c7918b1b8287657e
LakkinziMusic <lakkinzimusic@gmail.com>
2023-07-09 22:57:39 +0300

    AllStatusesEmail

---
+++ migrations/20230709195700_all_statuses_email.up.sql
@@ -0,0 +1,3 @@
+UPDATE public.form_statuses SET send_email = true WHERE id = 'a70abfa4-cd3a-46e5-9f59-810ed4e5d5af';
+UPDATE public.form_statuses SET send_email = true WHERE id = '43a69584-933b-4064-8643-1a2cc552b811';
+UPDATE public.form_statuses SET send_email = true WHERE id = '03977c2f-31a9-4160-b194-5e595d89e434';
