bd942ca4575eba9dd373cc0a16ff46957010f902
LakkinziMusic <lakkinzimusic@gmail.com>
2023-07-17 09:00:13 +0300

    SyncMigrations

--- migrations/20230716063505_address_ids.up.sql
+++ migrations/20230716063505_address_ids.up.sql
@@ -1,4 +1,4 @@
 alter table address_infos add column region_id varchar;
 alter table address_infos add column city_id varchar;
 alter table address_infos add column street_id varchar;
-alter table address_infos add column building_id varchar;
+alter table address_infos add column b_id varchar;
