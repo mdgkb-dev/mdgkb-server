1fe88457175742adb3c082ea50928bc3e99a7e2d
LakkinziMusic <lakkinzimusic@gmail.com>
2023-12-26 10:08:48 +0300

    FtspInit

---
+++ models/FTSPQuery.go
@@ -0,0 +1,11 @@
+package models
+
+import "github.com/pro-assistance/pro-assister/sqlHelper"
+
+type FTSPQuery struct {
+	QID  string         `json:"qid"`
+	FTSP sqlHelper.FTSP `json:"ftsp"`
+}
+type FTSPAnswer struct {
+	Data interface{} `json:"data"`
+}
