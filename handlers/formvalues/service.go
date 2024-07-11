e6bedfce7af2fac9b870242d1253f0dda5334005
LakkinziMusic <lakkinzimusic@gmail.com>
2024-07-11 14:52:41 +0300

    Service

--- handlers/formvalues/service.go
+++ handlers/formvalues/service.go
@@ -15,11 +15,11 @@ import (
 )
 
 func (s *Service) Upsert(item *models.FormValue) error {
-	err := users.S.UpsertEmail(context.TODO(), item.User)
-	if err != nil {
-		return err
-	}
-
+	// err := users.S.UpsertEmail(context.TODO(), item.User)
+	// if err != nil {
+	// 	return err
+	// }
+	var err error
 	item.User, err = users.S.Get(context.TODO(), item.User.ID.UUID.String())
 	if err != nil {
 		return err
